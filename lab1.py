c53a7919944a9895db820b79b02667e093460336
Pili Valenzuela <mdvalenzuela2@miuandes.cl>
2021-03-23 13:09:03 -0300

    first commit

--- lab1.py
+++ lab1.py
@@ -1,1 +1,10 @@
-print ("HOLA")
+import random
+
+tablero = []
+cantidad_cartas = "Ingrese la cantidad de cartas: "
+
+#orden del tablero depende de la cantidad de cartas
+
+#necesitamos crear un tablero de NxN
+def crear_tablero():
+    
