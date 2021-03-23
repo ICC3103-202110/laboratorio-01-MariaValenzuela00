5e5c4da49d397f574cbd030185591d4d6c409a45
Pili Valenzuela <mdvalenzuela2@miuandes.cl>
2021-03-23 13:48:06 -0300

    second commmit

--- lab1.py
+++ lab1.py
@@ -2,9 +2,18 @@ import random
 
 tablero = []
 cantidad_cartas = "Ingrese la cantidad de cartas: "
+print (cantidad_cartas)
 
 #orden del tablero depende de la cantidad de cartas
+ordentablero = cantidad_cartas
 
 #necesitamos crear un tablero de NxN
 def crear_tablero():
-    
+    for lines in range (ordentablero):
+        line = []
+
+        for columns in range (ordentablero):
+            line.append ("*")    
+
+    tablero.append(line)
+
