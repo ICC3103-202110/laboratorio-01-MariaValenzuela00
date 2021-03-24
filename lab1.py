72f459d5d82bb9a12975139dd98ad2bdd806d6e6
Pili Valenzuela <mdvalenzuela2@miuandes.cl>
2021-03-24 20:44:12 -0300

    five commit

--- lab1.py
+++ lab1.py
@@ -1,48 +1,73 @@
-import random
+from numpy import random
 
-cantidad_cartas = int(input("Ingrese la cantidad de cartas: "))
-print (cantidad_cartas)
+card_number = int(input("Ingrese la cantidad de cartas: "))
+print (card_number)
 
-cartas = cantidad_cartas*2
+cards = card_number*2
+div = cards // 6
 
 #necesitamos crear un tablero de NxN
-def crear_tablero(rows,columns):
+def board(rows,columns):
+    board = []
     for i in range (rows + 1):
         line_1 = " "
         line_2 = " "
+        row_board = []
 
         for j in range (columns):
             line_1 +=  " " + str (j)
             if i != 0:
                 line_2 +=  "_ " 
-
         
-        if i == 0:
-            print (line_1)
+            elif i == 0:
+                row_board.append (line_1)
 
-        else:
-            print( str(i-1) + line_2 )   
-    return crear_tablero
+            else:
+                row_board.append (str(i-1) + line_2 )  
 
-x = crear_tablero(cartas,cartas)
+        board.append(row_board) 
+    return board
+
+b = board (div, 6)
 
 #necesitamos que se agregen los numeros al tablero
-def num_tablero (num):
-    nums = []
-    a = 0
-    for i in range (cartas):
-        nums.append ([])
-
-        for j in range (cartas):
-            nums[i].append(num[a])
-            a += 1
-
-    for i in range (len(nums)):
-        print(nums[i])
+def num_board (div):
+    cards_values = []
+    nums = [] #matriz
     
+    for i in range (2):
+        row = []
+        repeat = 0
+
+        while repeat < 5:
+            x = random.randint(1, card_number)
+
+            if cards_values.count(x) < 2:
+                cards_values.append(x)
+                row.append(x)
+                repeat += 1
+
+            elif len(cards_values) == cards:
+                row.append("_ ")
+
+            else:
+                continue
+
     return nums
 
+num_board(div)
+
 #debemos mostrar las cartas que elige cada jugador
-#def mostrar_cartas (a,b):
+def card_show (x, y, board, nums):
+    board [y - 1][x - 1] = nums [x - 1][y - 1]
+    for i in board:
+        for j in i:
+            print (j, " ", end = "")
+        
+        print()
+    
+    return board [y - 1][x - 1]
+
+
 
 
